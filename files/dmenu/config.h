1e4377f4df2e00a08ddb385350e3ad4974371529
InigoGutierrez <inigogf.95@gmail.com>
2020-07-09 00:42:03 +0200

    Options for centered and bordered dmenu.

--- files/dmenu/config.h
+++ files/dmenu/config.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"mononoki:size=14",
@@ -22,3 +24,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
